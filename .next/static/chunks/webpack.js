24d2e96b38937297b07820bd0ea3568931fb5146
Alexis Quintuna <alexisiquintuna@gmail.com>
2022-01-17 15:04:04 -0500

    fixed fallback

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "736644ab14bfa01d19dd";
+/******/ 	var hotCurrentHash = "62030d8032e4610a9983";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -25,7 +25,7 @@ export async function getStaticPaths() {
   client.close();
 
   return {
-    fallback: false,
+    fallback: 'blocking',
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
